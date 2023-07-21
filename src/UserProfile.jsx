94d023cc9593c6e80802bef7c6562662b5810491
SammyMusyoki <75688249+SammyMusyoki@users.noreply.github.com>
2023-07-22 01:58:01 +0300

    1 file modified (button-fixes)

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,22 +1,18 @@
-import React, {useContext} from 'react';
-import { UserContext } from "./App"
+import React, { useContext } from 'react';
+import { UserContext } from './App';
 
 const UserProfile = () => {
-   const {
-    username,
-    email,
-    updateUser
-   } = useContext(UserContext)
+  const { username, email, updateUser } = useContext(UserContext);
   return (
     <div>
       <h2>My Profile</h2>
       <p>Username: {username}</p>
       <p>Email: {email}</p>
-      <button onClick={() => updateUser("Charles", "Charles@yahoo.com")}>
-        Update Username
+      <button onClick={() => updateUser('Charles', 'Charles@yahoo.com')}>
+        Update User
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default UserProfile;
